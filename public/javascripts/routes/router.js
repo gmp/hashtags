55bc0250442f0c6b3c4bf86965513712e05ab053
Greg Palmer <gpalmer323@gmail.com>
2013-06-26 14:31:27 -0700

    backbone router notes

--- public/javascripts/routes/router.js
+++ public/javascripts/routes/router.js
@@ -4,20 +4,30 @@ ht.Routes.Router = Backbone.Router.extend({
     "/": "login",
     "error": "error",
     "lobby/:userId": "lobby",
+    "lobby/:userId/search": "newGame",
     "game/:gameId": "game"
   },
 
   login: function() {
+    // create login view
     new ht.Views.Login();
   },
 
   error: function() {
+    // if login fails, create error view with option to try login again
     new ht.Views.Error();
   },
 
   lobby: function(userId) {
-    // find user in db and create model
+    // if login is successful, user is created in DB on server
+    // client finds user in DB based on ID and creates model to display lobby view
     new ht.Views.Lobby({model: user});
+  },
+
+  newGame: function(userId) {
+    // client syncs/downloads users collection to allow for searching
+    // start create game
+    new ht.Views.CreateGame({model: user});
   },
 
   game: function(gameId) {
