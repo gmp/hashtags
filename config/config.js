451736417ee2d4f641e861f94fd8e2d997ea12b6
Dylan O'Carroll <dylancocarroll@gmail.com>
2013-06-26 13:34:49 -0700

    favicon

---
+++ config/config.js
@@ -0,0 +1,21 @@
+var express = require('express')
+  , http = require('http')
+  , path = require('path');
+
+module.exports = function() {
+  var app = express();
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon(__dirname + '/public/images/hashtag.png'));
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  return app;
+};
