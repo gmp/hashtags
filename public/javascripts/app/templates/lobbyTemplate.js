4bb2792568f450ee82c6555d6909417cbc49c8b1
Greg Palmer <gpalmer323@gmail.com>
2013-06-27 14:40:21 -0700

    new game modal working

--- public/javascripts/app/templates/lobbyTemplate.js
+++ public/javascripts/app/templates/lobbyTemplate.js
@@ -3,12 +3,17 @@ ht.Templates.LobbyTemplate = _.template(''+
     '<div class="lobby-header">'+
       '<img class="avatar" src="<%= avatarURL %>">'+
       '<h1>#Lobby</h1>'+
-      '<button class="new-game">+</button><br>'+
+      '<button data-modal-id="new-game-modal" class="new-game-button">+</button><br>'+
     '</div>'+
     '<% if (games.length) { %>'+
       '<div class="games-container">Some games should go here. Possibly a gamesListView??</div>'+
     '<% } else { %>'+
       '<div class="intro"><p>To get the party started, begin  a new game or join a random game</div>'+
     '<% } %>'+
+    '<div id="new-game-modal" class="new-game-modal">'+
+      '<button id="start-new-game">Start a new game</button><br>'+
+      '<button id="join-random">Join a random game</button><br>'+
+      '<button id="cancel">Cancel</button>'+
+    '</div>'+
   '</div>'+
 '');
--- public/javascripts/app/views/lobbyView.js
+++ public/javascripts/app/views/lobbyView.js
@@ -6,9 +6,33 @@ ht.Views.LobbyView = Backbone.View.extend({
     this.render();
   },
 
+  events: {
+    'click .new-game-button': 'modalShow',
+    'click #cancel, #mask': 'modalHide',
+    'click #start-new-game': 'createGame'
+  },
+
   render: function() {
-    console.log(this.model);
     this.$el.empty();
     this.$el.append(this.template(this.model.attributes));
+  },
+
+  modalShow: function(event) {
+    var modalId = $(event.target).data('modal-id'),
+        $modal = $('#'+modalId);
+    $modal.fadeIn(300);
+    this.$el.append('<div id="mask" class="mask"></div>');
+    $('#mask').fadeIn(300);
+  },
+
+  modalHide: function() {
+    $('#mask, #new-game-modal').fadeOut(300, function() {
+      $('#mask').remove();
+    });
+  },
+
+  createGame: function() {
+    this.modalHide();
+    console.log('create game clicked');
   }
 });
