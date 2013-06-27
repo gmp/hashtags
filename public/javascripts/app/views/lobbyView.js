d49cdd12314acb1b74f315728defaccce70777db
Dylan O'Carroll <dylancocarroll@gmail.com>
2013-06-27 15:15:57 -0700

    reverted to 2 commits ago

--- public/javascripts/app/views/lobbyView.js
+++ public/javascripts/app/views/lobbyView.js
@@ -6,12 +6,6 @@ ht.Views.LobbyView = Backbone.View.extend({
     this.render();
   },
 
-  events: {
-    'click button' : function(){
-      app.navigate('game/' + this.model.attributes.games[0]);
-    }
-  },
-
   render: function() {
     console.log(this.model);
     this.$el.empty();
