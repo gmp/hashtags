0b78c644785f2a257ad379ca50adfa79377a8fcc
Greg Palmer <gpalmer323@gmail.com>
2013-06-27 12:16:16 -0700

    lobby view template

--- public/javascripts/app/templates/lobbyTemplate.js
+++ public/javascripts/app/templates/lobbyTemplate.js
@@ -6,9 +6,8 @@ ht.Templates.LobbyTemplate = _.template(''+
       '<button class="new-game">+</button><br>'+
     '</div>'+
     '<% if (games.length) { %>'+
-      '<div>Some games should go here</div>'+
+      '<div class="games-container">Some games should go here. Possibly a gamesListView??</div>'+
     '<% } else { %>'+
-      '<img class="arrow" src="http://www.clker.com/cliparts/Z/r/U/H/8/O/embedded-blue-arrow-point-left-hi.png">'+
       '<div class="intro"><p>To get the party started, begin  a new game or join a random game</div>'+
     '<% } %>'+
   '</div>'+
