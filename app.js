e4d8809030b11f00d74c516c2a38f809262c3e12
Eric <Ericrius1@gmail.com>
2013-06-26 19:20:17 -0700

    old

--- app.js
+++ app.js
@@ -6,8 +6,33 @@ var controller = require('./routes/controller.js'),
 
 
 var app = config();
+console.log(database);
+var Schema = database.mongoose.Schema;
+var Player = {
+  //Or selected hash tag, waiting, judge, ended
+  status: 'isFresh',
+  imageURL: undefined,
 
-database.setUpDatabase();
+}
+var GameSchema = new Schema({
+  title: String,
+  players: []
+})
+
+var UserSchema = new Schema({
+  username: String,
+  accessToken: String,
+  looking: false,
+  avatarURL: String
+});
+
+var Game = module.mongoose.model('GameModel', GameSchema);
+var game = new Game();
+game.title = "Test Title";
+game.save(function(err) {
+  if (err) console.log(err);
+  else console.log('success!');
+});
 
 
 
--- db/database.js
+++ db/database.js
@@ -1,39 +1,12 @@
-exports.setUpDatabase = function(){
 
 
-  var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
-  var config = {
-      // MongoDB endpoint
-      mongoDb: process.env.HASH_MONGO_URI,
-  };
+var config = {
+  // MongoDB endpoint
+  mongoDb: process.env.HASH_MONGO_URI,
+};
 
-  mongoose.connect(config.mongoDb);
+mongoose.connect(config.mongoDb);
+module.exports = mongoose;
 
-  var Schema = mongoose.Schema;
-  var Player = {
-    //Or selected hash tag, waiting, judge, ended
-    status: 'isFresh',
-    imageURL : undefined,
-
-  }
-  var Game = new Schema({
-     title  : String,
-     players : []
-  })
-
-  var User = new Schema({
-    username: String,
-    accessToken: String,
-    looking: false,
-    avatarURL: String
-  });
-
-  var MyGame = mongoose.model('GameModel', Game);
-  var game = new MyGame();
-  game.title = "Test Title";
-  game.save(function(err){
-      if(err)console.log(err);
-      else console.log('success!');
-  });
-}
