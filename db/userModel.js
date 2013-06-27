fd9405cc811de66fbc53871ce70d1854bbb95aee
Walker Flynn <walker.d.flynn@gmail.com>
2013-06-26 18:03:53 -0700

    saving a single user to the db

---
+++ db/userModel.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+  username: String,
+  accessToken: String,
+  looking: false,
+  avatarURL: String
+});
+
+module.exports = mongoose.model('UserModel', UserSchema);
