abb792cab5947d5964dac1c5bae5a87f62f09bc2
Walker Flynn <walker.d.flynn@gmail.com>
2013-06-28 21:54:21 -0700

    adding invitations to users. success not firing

--- models/inviteModel.js
+++ models/inviteModel.js
@@ -1,2 +1,12 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
+
+var InviteSchema = new Schema({
+  title     : String,
+  gameAdmin : String,
+  player2   : {user: String, accepted: {type: Boolean, default: false}},
+  player3   : {user: String, accepted: {type: Boolean, default: false}},
+  player4   : {user: String, accepted: {type: Boolean, default: false}},
+});
+
+module.exports = mongoose.model('Invites', InviteSchema);
