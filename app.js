c38d28a0f617f6f56d288e3d5146d853cc6e105d
Eric <Ericrius1@gmail.com>
2013-06-29 14:38:11 -0700

    basic socket stuff

--- app.js
+++ app.js
@@ -14,5 +14,12 @@ var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-io = require('socket.io').listen(server);
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('message', { hello: 'world' });
+  socket.on('event', function (data) {
+    console.log(data);
+  });
+});
 
