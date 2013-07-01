a7cb19c886188f85b9dd747318d6ce53338228ad
Greg Palmer <gpalmer323@gmail.com>
2013-07-01 15:32:05 -0700

    clicking into game not working

--- public/javascripts/app/templates/lobbyGamesListTemplate.js
+++ public/javascripts/app/templates/lobbyGamesListTemplate.js
@@ -2,7 +2,7 @@ ht.Templates.LobbyGamesListTemplate = _.template(''+
   '<div class="section-container accordian" data-section="accordian">'+
     '<% for(var i = 0; i < games.length; i++) { %>'+
       '<section>'+
-        '<p class="title game" data-section-title><i class="icon-reorder menu-icon"></i>| <%= games[i].title %><i class="icon-chevron-right right-icon"></i></p>'+
+        '<p class="title game" data-section-title><i class="icon-reorder menu-icon"></i> | <%= games[i].title %></p>'+
         '<div class="content" data-section-content>'+
           '<ul class="no-bullet">'+
             '<li>Prompt: <%= games[i].prompt %></li>'+
@@ -11,7 +11,7 @@ ht.Templates.LobbyGamesListTemplate = _.template(''+
             // '<% for(var j = 0; j < games[i].players.length; j++) { %>'+
             //   '<li>games.[i].players[j].username</li>'+
             // '<% } %>'+
-            '<li><a href="/#game/<%= games[i]._id %>">Play!</a></li>'+
+            '<li><a class="button tiny" href="/#game/<%= games[i]._id %>">Play!</a></li>'+
           '</ul>'+
         '</div>'+
       '</section>'+
