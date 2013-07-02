171e23e4c9870b626384948ee10577b97fa7e860
Dylan O'Carroll <dylancocarroll@gmail.com>
2013-07-01 21:01:03 -0700

    twerkin on judgeview

--- public/javascripts/app/views/judgeView.js
+++ public/javascripts/app/views/judgeView.js
@@ -6,12 +6,36 @@ ht.Views.JudgeView = Backbone.View.extend({
 
   initialize: function() {
     this.render();
+    _.bindAll(this, 'playerSubmit');
+    ht.dispatcher.on('playerSubmit', this.playerSubmit);
   },
 
-// 3 states: waiting for everyones photos.  ready to choose (3 pics come up at once). game end.
+  events: {
+    'click img': 'judgeChoose',
+    'click video': 'judgeChoose'
+  },
 
   render: function() {
-    this.$el.append(this.template());
+    this.$el.empty();
+    console.log("players: ", this.model.get('players'));
+    this.$el.append(this.template({players: this.model.get('players')}));
+  },
+
+  playerSubmit: function() {
+    var self = this;
+    console.log("GameWaitingView");
+    this.model.fetch({
+      success: function(){
+        self.render();
+      },
+      error: function(){
+        console.error("error");
+      }
+    });
+  },
+
+  judgeChoose: function() {
+    console.log("judge chooses: ");
   }
 
 });
