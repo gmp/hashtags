5eddc6d43782ab0b71f9738ad0cb9ec553a9f79d
Greg Palmer <gpalmer323@gmail.com>
2013-07-01 18:46:19 -0700

    game waiting view

--- public/javascripts/app/templates/gameWaitingTemplate.js
+++ public/javascripts/app/templates/gameWaitingTemplate.js
@@ -1,11 +1,19 @@
 ht.Templates.GameWaitingTemplate = _.template(''+
   '<% _.each(players, function(player) { %>'+
     '<% if(player.submitted) { %>'+
-      '<% if(player.submission.type === "image") { %>'+
-        '<img src="<%= player.submission.url %>">'+
-      '<% } else { %>'+
-        '<video src="<%= player.submission.url %>"></video>'+
-      '<% } %>'+
+      '<div class="row text-center">'+
+        '<div class="small-11 small-centered columns">'+
+          '<% if(player.submission.type === "image") { %>'+
+            '<h4><%= player.username %> submitted:</h4>'+
+            '<img class="media" src="<%= player.submission.url %>">'+
+            '<h4><%= player.submission.hashtag %></h4>'+
+          '<% } else { %>'+
+            '<h4><%= player.username %> submitted:</h4>'+
+            '<video class="media" src="<%= player.submission.url %>"></video>'+
+            '<h4><%= player.submission.hashtag %></h4>'+
+          '<% } %>'+
+        '</div>'+
+      '</div>'+
     '<% } %>'+
   '<% }); %>'+
 '');
