c46f96c9ba628d540240410cb6c821f982283735
Eric <Ericrius1@gmail.com>
2013-07-04 15:03:47 -0700

    saving game when all players accept an invitation

--- controllers/inviteController.js
+++ controllers/inviteController.js
@@ -1,93 +1,140 @@
 var User = require('../models/userModel.js'),
-    _ = require('underscore'),
-    Invite = require('../models/inviteModel.js'),
-    clients = require('../config/socketEvents.js').clients;
+  Game = require('../models/gameModel.js'),
+  _ = require('underscore'),
+  Invite = require('../models/inviteModel.js'),
+  clients = require('../config/socketEvents.js').clients;
 
-exports.create = function(req, res){
+exports.create = function(req, res) {
   var obj = req.body;
   var invite = new Invite();
   invite.title = obj.title;
   invite.author = obj.author;
   invite.gameAdmin = obj.gameAdmin;
-  invite.player2 = {user: obj.player2, accepted: 'waiting'};
-  invite.player3 = {user: obj.player3, accepted: 'waiting'};
-  invite.player4 = {user: obj.player4, accepted: 'waiting'};
-  _.each(obj, function(value, key){
+  invite.player2 = {
+    user: obj.player2,
+    accepted: 'waiting'
+  };
+  invite.player3 = {
+    user: obj.player3,
+    accepted: 'waiting'
+  };
+  invite.player4 = {
+    user: obj.player4,
+    accepted: 'waiting'
+  };
+  _.each(obj, function(value, key) {
     console.log(key, value);
-    if(key !== 'title'  && key !== 'author'){
-      User.findById(value, function(err, obj){
-        if(err) console.log(err);
-        if(key === 'gameAdmin'){
-          if(!obj.pendingGames){
+    if (key !== 'title' && key !== 'author') {
+      User.findById(value, function(err, obj) {
+        if (err) console.log(err);
+        if (key === 'gameAdmin') {
+          if (!obj.pendingGames) {
             obj.pendingGames = [];
           }
-          obj.pendingGames.push({invite: invite._id, title: invite.title, waitingOn: 3});
+          obj.pendingGames.push({
+            invite: invite._id,
+            title: invite.title,
+            waitingOn: 3
+          });
         } else {
-          if(!obj.invites){
+          if (!obj.invites) {
             obj.invites = [];
           }
-          obj.invites.push({invite: invite._id, author: invite.author});
-          if(clients[obj._id]){
+          obj.invites.push({
+            invite: invite._id,
+            author: invite.author
+          });
+          if (clients[obj._id]) {
             clients[obj._id].emit('changeInUser');
           }
         }
-        obj.save(function(err){
-          if(err) console.log(err);
+        obj.save(function(err) {
+          if (err) console.log(err);
         });
       });
     }
   });
-  invite.save(function(err){
-    if(err) console.log(err);
+  invite.save(function(err) {
+    if (err) console.log(err);
     res.writeHead(204);
     res.end();
   });
 };
 
-exports.accept = function(req, res){
+exports.accept = function(req, res) {
   console.log(req.body)
   var inviteId = req.body.inviteId;
-  var userId  = req.body.userId;
+  var userId = req.body.userId;
   //grab invite from database
-  Invite.findById(inviteId, function (err, invite){
+  Invite.findById(inviteId, function(err, invite) {
     var playersArr = [];
     playersArr.push(invite.player2);
     playersArr.push(invite.player3);
     playersArr.push(invite.player4);
-    for(var i = 0; i < playersArr.length; i ++){
-      if(playersArr[i].user === userId){
-        invite.set('player' + (i+2) +'.accepted', 'accepted');
-        invite.set('waitingOn', --invite.waitingOn);
-        moveGameToPending(playersArr[i].user, inviteId, invite.title, invite.waitingOn, res);
+    for (var i = 0; i < playersArr.length; i++) {
+      if (playersArr[i].user === userId) {
+        invite.set('player' + (i + 2) + '.accepted', 'accepted');
+        console.log("WAITING ON",invite.waitingOn);
+        if (invite.waitingOn > 1) {
+          invite.set('waitingOn', --invite.waitingOn);
+          moveGameToPending(playersArr[i].user, inviteId, invite.title, invite.waitingOn, res);
+        } else if (invite.waitingOn === 1) {
+          createGame(invite.gameAdmin, playersArr);
+        }
+
       }
     }
-    invite.save(function (err, invite){
-       if(err) console.log(err);
+    invite.save(function(err, invite) {
+      if (err) console.log(err);
     });
   });
 };
 
-var moveGameToPending = function (userId, inviteId, title, waitingOn, res){
-  User.findById(userId, function (err, user){
-    for(var i = 0; i < user.invites.length; i ++){
-      if(user.invites[i].invite.toString() === inviteId){
+//If waitingOn === 1:
+//Create a game instead of move game to pending 
+//Add that game to every user involved in game
+// Remove pending game from every user including game admin except for one who is currently accepting
+//Then clients[userId].emit on each user- in the save callback of that user
+var createGame = function(gameAdmin, otherPlayerIds) {
+  console.log("new game");
+  var game = new Game();
+  game.title = "User Created Game!";
+  game.prompt = "When I woke up from a nap my siginificant other had ___";
+  game.players = {};
+  User.findById(gameAdmin, function(err, admin) {
+    game.players[gameAdmin._id] = gameAdmin;
+  });
+  for (var i = 0; i < otherPlayerIds; i++) {
+    User.findById(otherPlayersIds[i], function(err, player) {
+      game.players[player._id] = player;
+    });
+  }
+
+  game.save(function(err) {
+    if (err) console.log(err);
+  });
+
+};
+
+var moveGameToPending = function(userId, inviteId, title, waitingOn, res) {
+  User.findById(userId, function(err, user) {
+    for (var i = 0; i < user.invites.length; i++) {
+      if (user.invites[i].invite.toString() === inviteId) {
         user.invites.splice(i, 1);
         user.set('invites', user.invites);
       }
     };
     var pendingGames = [];
-    pendingGames.push({invite: inviteId, title: title, waitingOn: waitingOn});
+    pendingGames.push({
+      invite: inviteId,
+      title: title,
+      waitingOn: waitingOn
+    });
     user.set('pendingGames', pendingGames);
-    user.save(function(err){
-      if(err) console.log(err);
+    user.save(function(err) {
+      if (err) console.log(err);
       res.writeHead(204);
       res.end();
     });
   });
 }
-
-
-
-
-
-
