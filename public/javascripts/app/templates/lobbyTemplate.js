22f70b245be5ed4a1c96ec802f1b6b770193e1b3
Greg Palmer <gpalmer323@gmail.com>
2013-07-08 10:18:23 -0700

    no games in lobby with styles

--- public/javascripts/app/templates/lobbyTemplate.js
+++ public/javascripts/app/templates/lobbyTemplate.js
@@ -10,6 +10,13 @@ ht.Templates.LobbyTemplate = _.template(''+
       '<button data-modal-id="new-game-modal" id="new-game-button" class="small button">+</button><br>'+
     '</div>'+
   '</div>'+
+  '<% if (games.length === 0 && invites.length === 0 && pendingGames.length === 0) { %>'+
+    '<div class="row noGames">'+
+      '<div class="small-10 small-centered columns">'+
+        '<p>You have no games!<br><br>Click the [+]<br>to get started</p>'+
+      '</div>'+
+    '</div>'+
+  '<% } %>'+
   '<div id="new-game-modal" class="new-game-modal">'+
     '<div class="small-centered columns">'+
       '<button class="small" id="start-new-game">Start a new game</button><br>'+
