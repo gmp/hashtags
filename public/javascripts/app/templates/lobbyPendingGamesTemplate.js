2548b1f4ad1b4862855080e4dc5be509a5adee9a
Greg Palmer <gpalmer323@gmail.com>
2013-07-10 14:38:18 -0700

    fix pending games strikethrough and random game controller

--- public/javascripts/app/templates/lobbyPendingGamesTemplate.js
+++ public/javascripts/app/templates/lobbyPendingGamesTemplate.js
@@ -2,9 +2,13 @@ ht.Templates.LobbyPendingGamesTemplate = _.template(''+
   '<div class="section-container accordian" data-section="accordian">'+
     '<% for(var i = 0; i < pendingGames.length; i++) { %>'+
       '<section>'+
-        '<p class="title" data-section-title>Pending game: <%= pendingGames[i].title %></p>'+
-        '<div class="content text-center" data-section-content>'+
-          '<% if (pendingGames[i].declined) { %>'+
+        '<% if (pendingGames[i].declined) { %>'+
+          '<p class="title strike" data-section-title>Pending game: <%= pendingGames[i].title %></p>'+
+          '<% } else { %>' +
+          '<p class="title" data-section-title>Pending game: <%= pendingGames[i].title %></p>'+
+          '<% } %>' +
+          '<div class="content" data-section-content>'+
+        '<% if (pendingGames[i].declined) { %>'+
             '<p class="red">Ugh! One of your stupid friends decided not to play.</p>'+
             '<button class="declined small expand" id="<%=pendingGames[i]._id%>">Remove game</button>'+
           '<% } else { %>' +
