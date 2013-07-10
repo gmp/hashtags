c02df0d5ca71a4cb5e4fe7e04302619b50096dea
Walker Flynn <walker.d.flynn@gmail.com>
2013-07-10 16:35:58 -0700

    environment variables

--- instaOauth/authenticate.js
+++ instaOauth/authenticate.js
@@ -12,8 +12,8 @@ passport.deserializeUser(function(obj, done) {
 
 //LOCAL
 passport.use(new InstagramStrategy({
-    clientID: 'd37dfbd483674207b72a447e1b63b6bc',
-    clientSecret: '5e0d4975780a4e36a59d78cb01c74868',
+    clientID: HASHTAGS_CLIENT_ID,
+    clientSecret: HASHTAGS_CLIENT_SECRET,
     callbackURL: "/auth/instagram/callback"
   },
 
